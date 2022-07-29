8b0eada5adad42e5d9572b5377d4ec668b4d981b
Makraiyassin <makrai.yassin@gmail.com>
2022-07-29 09:36:58 +0200

    api

--- src/main/java/be/digitalcity/introspring/controllers/RestControllerTest.java
+++ src/main/java/be/digitalcity/introspring/controllers/RestControllerTest.java
@@ -3,14 +3,17 @@ package be.digitalcity.introspring.controllers;
 import be.digitalcity.introspring.dto.PlayerDTO;
 import be.digitalcity.introspring.services.PlayerService;
 import be.digitalcity.introspring.utils.Poste;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping( "/api")
 public class RestControllerTest {
     private final PlayerService playerService;
 
@@ -18,8 +21,13 @@ public class RestControllerTest {
         this.playerService = playerService;
     }
 
-    @GetMapping("/api")
+    @GetMapping("/players")
     public List<PlayerDTO> getPlayer(){
         return this.playerService.getAll();
     }
+    @RequestMapping(path = "/first")
+    public ResponseEntity<String> firstWebApiCall(){
+        return ResponseEntity.ok("hello");
+    }
+
 }
